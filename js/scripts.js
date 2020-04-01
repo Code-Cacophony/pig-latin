aeaec509ff6ce230427986b07cdb5c0afc84b10a
Mikah Mathews <mikah@outofband.org>
2020-04-01 16:19:26 -0700

    It will add working js to separate words into strings in arrays

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,20 @@ function isVowel(char) { //to determine if letter is a variable
 }
 
 function splitInput(input){ //splits input
-  return input.split(",");
+  return input.split(" ");
 }
 
-function splitLetter(word){
-  return word.split(",");
+function splitLetter(words){
+  newArray = [];
+  // var myString = document.getElementById("first").innerHTML;
+  for (var i = 0; i < words.length; i++) {
+    newArray.push(words[i].split(""));
+    // alert(words[i]);
+  }
+  return newArray;
 }
+
+//have a loop for each word in split words - console.log the word then loop
 
 $(document).ready(function(){
   $("#results").hide();
@@ -16,6 +24,7 @@ $(document).ready(function(){
     event.preventDefault();
     var userInput = $("#sentence").val();
     var splitWords = splitInput(userInput);
+    console.log(splitWords);
     var splitLetters = splitLetter(splitWords);
 
 
@@ -27,3 +36,4 @@ $(document).ready(function(){
     $("#results").show();
   })
 })
+
